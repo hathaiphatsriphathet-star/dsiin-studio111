a9f003e1b00b2f5ff7b31903cf4d75940dd3a1c5
hathaiphatsriphathet-star <hathaiphatsriphathet@gmail.com>
2026-03-27 21:49:48 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Configuration - Dsiin Studio
+const firebaseConfig = {
+  apiKey: "AIzaSyCd7j3TnUYvHA9tqrjdjG-gpH0OWljIdpQ",
+  authDomain: "dsiinstodio.firebaseapp.com",
+  projectId: "dsiinstodio",
+  storageBucket: "dsiinstodio.firebasestorage.app",
+  messagingSenderId: "68791849791",
+  appId: "1:68791849791:web:631e6ace47b407970e398a",
+  measurementId: "G-337QVW3CL4"
+};
+
+firebase.initializeApp(firebaseConfig);
